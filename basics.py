3075f6ecb27f21259fc6a941e1b4d2e3c5e6e7d2
krystianopryszek99 <kopryszek@gmail.com>
2021-02-11 21:55:00 +0000

    Looked at the basics.

---
+++ basics.py
@@ -0,0 +1,14 @@
+# Krystian Opryszek 
+# Pyrhon basics 
+# 2021-02-11
+
+print("Hello, world!")
+
+a = 1
+b = 1.0
+s = "Hello, world from a string!"
+t = 'Hello, from a different string'
+
+print(a, b, s, t)
+
+print (s[3:10:2]) 
