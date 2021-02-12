0bdafdfffe248bf0a00aaabd306b3382d70cc91a
krystianopryszek99 <kopryszek@gmail.com>
2021-02-12 21:42:32 +0000

    if statement/while loop

--- basics.py
+++ basics.py
@@ -1,47 +1,27 @@
 # Krystian Opryszek 
 # Pyrhon basics 
-# 2021-02-11
+# 2021-02-12
 
-#print("Hello, world!")
+def factorial(n):
+    """Number to calculate functional of."""
+    #deal with negative inputs
+    if n < 1:
+        m = -n
+    else:
+        m = n
+    # the running total - eventually the factional
+    total = 1
+    #loop to do the multiplications
+    while m > 0:
+        total = total * m
+        m = m - 1
+    #return the answer
+    if n < 1:
+        return -total
+    else:
+        return total
 
-a = 1
-b = 1.0
-s = "Hello, world from a string!"
-t = 'Hello, from a different string'
-
-#print(a, b, s, t)
-#print (s[3:10:2]) 
-
-# lists
-x = [1, 2, 3, "Hello", 1.0]
-#print(x)
-#print(x[0])
-#print(x[2])
-#print(x[-1])
-
-# for loop
-#for i in x[::2]:
-#    print(i)
-#    print(i + i)
-
-#for i in range(10):
-#    print(i)
-
-# creating dictionary
-d = {"no_wheels": 4, "make": "Skoda"}
-
-print(d["no_wheels"])
-
-d["model"] = "Superb"
-print(d["model"])
-
-#list comprehensions
-#create lists from other lists
-
-r = [1,2,3,4]
-
-print(r)
-
-s = [i*i for i in r]
-
-print(s)
+#test the function
+n = -20
+#calculate the factorial of n
+print(f"The functional of {n} is {factorial(n)}.")
